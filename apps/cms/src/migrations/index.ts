b4ce7d743530cea82a0a42bf4ac10fad67c3b4b6
Koki <kouki.fukuda@gmail.com>
2025-12-10 03:30:17 +0900

    Refactor code structure for improved readability and maintainability

---
+++ apps/cms/src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251209_181908 from './20251209_181908';
+
+export const migrations = [
+  {
+    up: migration_20251209_181908.up,
+    down: migration_20251209_181908.down,
+    name: '20251209_181908'
+  },
+];
